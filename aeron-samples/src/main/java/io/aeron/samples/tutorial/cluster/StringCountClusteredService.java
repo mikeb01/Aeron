bab08184610e10618ed859fcda2b6b9470956876
Michael Barker <mikeb01@gmail.com>
2020-01-09 13:44:16 +1300

    [Java] Add snapshot code to sample String count service.

--- aeron-samples/src/main/java/io/aeron/samples/tutorial/cluster/StringCountClusteredService.java
+++ aeron-samples/src/main/java/io/aeron/samples/tutorial/cluster/StringCountClusteredService.java
@@ -9,32 +9,40 @@ import io.aeron.cluster.service.ClusteredService;
 import io.aeron.logbuffer.Header;
 import org.agrona.BitUtil;
 import org.agrona.DirectBuffer;
-import org.agrona.ExpandableArrayBuffer;
+import org.agrona.ExpandableDirectByteBuffer;
+import org.agrona.MutableDirectBuffer;
 import org.agrona.collections.MutableInteger;
+import org.agrona.collections.MutableReference;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Consumer;
 
 // tag::new_service[]
 public class StringCountClusteredService implements ClusteredService
 // end::new_service[]
 {
-    private final ExpandableArrayBuffer egressMessageBuffer = new ExpandableArrayBuffer(4);
+    public static final int APPLICATION_HEADER_LENGTH = 2 * BitUtil.SIZE_OF_INT;
 
+    private final MutableDirectBuffer egressMessageBuffer = new ExpandableDirectByteBuffer(4);
+    private final MutableDirectBuffer snapshotHeaderBuffer = new ExpandableDirectByteBuffer(8);
+    private final MutableDirectBuffer snapshotMessageBuffer = new ExpandableDirectByteBuffer(1024 * 1024);
+    private final MutableDirectBuffer snapshotLoadBuffer = new ExpandableDirectByteBuffer(1024 * 1024);
+
+    // tag::state[]
     private final HashMap<String, MutableInteger> receivedStrings = new HashMap<>();
+    // end::state[]
+    private Cluster cluster;
 
     // tag::start[]
     public void onStart(final Cluster cluster, final Image snapshotImage)
     {
-        while (null != snapshotImage && !snapshotImage.isEndOfStream())  // <1>
+        this.cluster = cluster;
+        if (null != snapshotImage)
         {
-            final int fragmentsPolled = snapshotImage.poll((buffer, offset, length, header) ->
-            {
-                final int count = buffer.getInt(offset);
-                final String receivedString = buffer.getStringAscii(offset + BitUtil.SIZE_OF_INT);
-                receivedStrings.put(receivedString, new MutableInteger(count));
-            }, 10);
-
-            cluster.idle(fragmentsPolled);                              // <2>
+            loadSnapshot(cluster, snapshotImage);
         }
     }
     // end::start[]
@@ -49,6 +57,7 @@ public class StringCountClusteredService implements ClusteredService
 
     }
 
+    // tag::message[]
     public void onSessionMessage(
         final ClientSession session,
         final long timestamp,
@@ -57,32 +66,115 @@ public class StringCountClusteredService implements ClusteredService
         final int length,
         final Header header)
     {
-        final String receivedString = buffer.getStringUtf8(offset);
-        MutableInteger mutableInteger = receivedStrings.computeIfAbsent(receivedString, s -> new MutableInteger(0));
+        final long correlationId = buffer.getLong(offset);
+        final String key = buffer.getStringUtf8(offset + BitUtil.SIZE_OF_LONG);
+        final MutableInteger mutableInteger = receivedStrings.computeIfAbsent(key, s -> new MutableInteger(0));
         mutableInteger.value++;
 
-        egressMessageBuffer.putInt(0, mutableInteger.value);
+        egressMessageBuffer.putLong(0, correlationId);
+        egressMessageBuffer.putInt(BitUtil.SIZE_OF_LONG, mutableInteger.value);
 
-        // TODO: how does this work under replay?
-        while (session.offer(egressMessageBuffer, 0, BitUtil.SIZE_OF_INT) < 0)
+        if (null != session)
         {
-            if (Thread.currentThread().isInterrupted())
+            while (session.offer(egressMessageBuffer, 0, BitUtil.SIZE_OF_LONG + BitUtil.SIZE_OF_INT) < 0)
             {
-                break;
+                cluster.idle();
             }
-            Thread.yield();
         }
     }
+    // end::message[]
 
     public void onTimerEvent(final long correlationId, final long timestamp)
     {
 
     }
 
+    // tag::takeSnapshot[]
     public void onTakeSnapshot(final ExclusivePublication snapshotPublication)
     {
+        final MutableInteger position = new MutableInteger(0);
 
+        snapshotMessageBuffer.putInt(position.value, receivedStrings.size());
+        position.value += BitUtil.SIZE_OF_INT;
+        receivedStrings.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(e ->
+        {
+            final int encodedLength = encodeStringUtf8(snapshotMessageBuffer, position.value, e.getKey());
+            position.value += encodedLength;
+            snapshotMessageBuffer.putInt(position.value, e.getValue().value);
+            position.value += BitUtil.SIZE_OF_INT;
+        });
+
+        final int totalMessageLength = position.value;
+        final int maxMessageLength = snapshotPublication.maxPayloadLength() - APPLICATION_HEADER_LENGTH;
+
+        int offset = 0;
+        while (offset < totalMessageLength)
+        {
+            snapshotHeaderBuffer.putInt(0, offset);
+            snapshotHeaderBuffer.putInt(BitUtil.SIZE_OF_INT, totalMessageLength);
+
+            final int length = Math.min(maxMessageLength, totalMessageLength - offset);
+
+            while (snapshotPublication.offer(
+                snapshotHeaderBuffer, 0, APPLICATION_HEADER_LENGTH,
+                snapshotMessageBuffer, offset, length) < 0)
+            {
+                cluster.idle();
+            }
+
+            offset += length;
+        }
     }
+    // end::takeSnapshot[]
+
+    // tag::loadSnapshot[]
+    private void loadSnapshot(final Cluster cluster, final Image snapshotImage)
+    {
+        final MutableInteger messageOffset = new MutableInteger(0);
+        final MutableInteger messageLength = new MutableInteger(0);
+        final MutableInteger totalLength = new MutableInteger(0);
+
+        while (!snapshotImage.isEndOfStream())  // <1>
+        {
+            final int fragmentsPolled = snapshotImage.poll((buffer, offset, length, header) ->
+            {
+                messageOffset.value = buffer.getInt(offset);
+                totalLength.value = buffer.getInt(offset + BitUtil.SIZE_OF_INT);
+                messageLength.value = length - APPLICATION_HEADER_LENGTH;
+
+                snapshotLoadBuffer.putBytes(
+                    messageOffset.value, buffer, offset + APPLICATION_HEADER_LENGTH, messageLength.value);
+            }, 10);
+
+            if (0 < totalLength.value && messageOffset.value + messageLength.value  == totalLength.value)
+            {
+                int position = 0;
+                final int numberOfEntries = snapshotLoadBuffer.getInt(position);
+                final MutableReference<String> keyReference = new MutableReference<>();
+
+                position += BitUtil.SIZE_OF_INT;
+                while (position < totalLength.value)
+                {
+                    final int encodedLength = decodeStringUtf8(snapshotLoadBuffer, position, keyReference::set);
+                    position += encodedLength;
+                    int count = snapshotLoadBuffer.getInt(position);
+                    position += BitUtil.SIZE_OF_INT;
+
+                    receivedStrings.put(keyReference.ref, new MutableInteger(count));
+                }
+
+                assert receivedStrings.size() == numberOfEntries : "Sanity check size of map";
+
+                break;
+            }
+
+            cluster.idle(fragmentsPolled);      // <2>
+        }
+
+        assert messageOffset.value + messageLength.value == totalLength.value :
+            "Sanity check that snapshot is complete";
+    }
+    // end::loadSnapshot[]
 
     public void onRoleChange(final Cluster.Role newRole)
     {
@@ -92,5 +184,47 @@ public class StringCountClusteredService implements ClusteredService
     public void onTerminate(final Cluster cluster)
     {
 
+    }
+
+    private int encodeStringUtf8(final MutableDirectBuffer buffer, final int offset, final String s)
+    {
+        final byte[] bytes = s.getBytes(StandardCharsets.UTF_8);
+        buffer.putInt(offset, bytes.length);
+        buffer.putBytes(offset + BitUtil.SIZE_OF_INT, bytes);
+        return BitUtil.SIZE_OF_INT + bytes.length;
+    }
+
+    private int decodeStringUtf8(
+        final DirectBuffer buffer,
+        final int offset,
+        final Consumer<String> consumer)
+    {
+        final int length = buffer.getInt(offset);
+        final byte[] bytes = new byte[length];
+        buffer.getBytes(offset + BitUtil.SIZE_OF_INT, bytes);
+        consumer.accept(new String(bytes, StandardCharsets.UTF_8));
+        return BitUtil.SIZE_OF_INT + length;
+    }
+
+    public boolean equals(final Object o)
+    {
+        if (this == o)
+        { return true; }
+        if (o == null || getClass() != o.getClass())
+        { return false; }
+        final StringCountClusteredService that = (StringCountClusteredService)o;
+        return receivedStrings.equals(that.receivedStrings);
+    }
+
+    public int hashCode()
+    {
+        return Objects.hash(receivedStrings);
+    }
+
+    public String toString()
+    {
+        return "StringCountClusteredService{" +
+               "receivedStrings=" + receivedStrings +
+               '}';
     }
 }
---
+++ aeron-samples/src/test/java/io/aeron/samples/tutorial/cluster/StringCountClusteredServiceTest.java
@@ -0,0 +1,140 @@
+package io.aeron.samples.tutorial.cluster;
+
+import io.aeron.Aeron;
+import io.aeron.ExclusivePublication;
+import io.aeron.Image;
+import io.aeron.Subscription;
+import io.aeron.cluster.service.Cluster;
+import io.aeron.driver.MediaDriver;
+import io.aeron.driver.ThreadingMode;
+import org.agrona.BitUtil;
+import org.agrona.ExpandableArrayBuffer;
+import org.agrona.MutableDirectBuffer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.extension.TestWatcher;
+import org.mockito.invocation.InvocationOnMock;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static io.aeron.CommonContext.IPC_CHANNEL;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+
+class StringCountClusteredServiceTest
+{
+    public static final int STREAM_ID = 1001;
+
+    private final long seed = System.nanoTime();
+    private final Random rnd = new Random(seed);
+
+    @RegisterExtension
+    final TestWatcher testWatcher = new TestWatcher()
+    {
+        public void testFailed(final ExtensionContext context, final Throwable cause)
+        {
+            System.err.println(context.getDisplayName() + " failed with random seed: " + seed);
+        }
+    };
+
+    private final Cluster cluster = mock(Cluster.class);
+    //    private final List<String> messages = Arrays.asList("First Message", "Second Message");
+    private final List<String> messages = generateRandomMessages(rnd, 1000, 50);
+
+    @BeforeEach
+    void setUp()
+    {
+        doAnswer(StringCountClusteredServiceTest::idle).when(cluster).idle();
+    }
+
+    @Test
+    void shouldStoreAndLoadSnapshot() throws InterruptedException
+    {
+        final StringCountClusteredService serviceInitialState = new StringCountClusteredService();
+        final StringCountClusteredService serviceRestoredState = new StringCountClusteredService();
+
+        serviceInitialState.onStart(cluster, null);
+        final MutableDirectBuffer buffer = new ExpandableArrayBuffer(1024);
+
+        long correlationId = rnd.nextLong();
+        for (String message : messages)
+        {
+            buffer.putLong(0, correlationId++);
+            final int keyLength = buffer.putStringUtf8(BitUtil.SIZE_OF_LONG, message);
+            serviceInitialState.onSessionMessage(null, 0, buffer, 0, BitUtil.SIZE_OF_LONG + keyLength, null);
+        }
+
+        try (MediaDriver driver = MediaDriver.launchEmbedded(
+            new MediaDriver.Context().threadingMode(ThreadingMode.SHARED));
+            Aeron aeron = Aeron.connect(new Aeron.Context().aeronDirectoryName(driver.aeronDirectoryName()));
+            final ExclusivePublication snapshotPublication = aeron.addExclusivePublication(IPC_CHANNEL, STREAM_ID);
+            final Subscription subscription = aeron.addSubscription(IPC_CHANNEL, STREAM_ID))
+        {
+            while (!snapshotPublication.isConnected())
+            {
+                yieldAndCheckInterrupt();
+            }
+
+            while (subscription.hasNoImages())
+            {
+                yieldAndCheckInterrupt();
+            }
+
+            assertEquals(subscription.imageCount(), 1);
+
+            final Thread t = new Thread(() ->
+            {
+                final Image image = subscription.imageAtIndex(0);
+                serviceRestoredState.onStart(cluster, image);
+            });
+
+            t.setUncaughtExceptionHandler((t1, e) -> e.printStackTrace());
+            t.start();
+
+            serviceInitialState.onTakeSnapshot(snapshotPublication);
+            snapshotPublication.close();
+
+            t.join();
+
+            assertEquals(serviceInitialState, serviceRestoredState);
+        }
+    }
+
+    private static void yieldAndCheckInterrupt()
+    {
+        Thread.yield();
+        if (Thread.currentThread().isInterrupted())
+        {
+            fail("unexpected interrupt - test likely to have timed out");
+        }
+    }
+
+    private static List<String> generateRandomMessages(Random r, final int count, int length)
+    {
+        final List<String> strings = new ArrayList<>();
+        final byte[] b = new byte[length];
+        for (int i = 0; i < count; i++)
+        {
+            for (int j = 0; j < b.length; j++)
+            {
+                b[j] = (byte)(32 + r.nextInt(94));
+            }
+            strings.add(new String(b, StandardCharsets.US_ASCII));
+        }
+
+        return strings;
+    }
+
+    private static Object idle(InvocationOnMock invocation)
+    {
+        yieldAndCheckInterrupt();
+        return null;
+    }
+}
